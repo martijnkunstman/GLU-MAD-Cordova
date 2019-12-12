7fa300b6af6143a05a49280e4179e23072249dd4
martijnkunstman <martijn@kunstman.nl>
2019-12-12 21:17:45 +0100

    init cordova

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # GLU-MAD-Cordova
+
+https://cordova.apache.org/
+
+npm install -g cordova
+cordova create cordova
+cd cordova
+cordova platform
+cordova platform add android
+copy all files in cordova to root (exept node_modules)
+npm install
+cordova run android
+cordova plugin add cordova-plugin-battery-status
+
+
+
---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-battery-status.battery",
+      "file": "plugins/cordova-plugin-battery-status/www/battery.js",
+      "pluginId": "cordova-plugin-battery-status",
+      "clobbers": [
+        "navigator.battery"
+      ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification",
+      "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification_android",
+      "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-battery-status": "2.0.3",
+    "cordova-plugin-vibration": "3.1.1",
+    "cordova-plugin-dialogs": "2.0.2"
+  };
+});
